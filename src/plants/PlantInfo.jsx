f95ad685f19c1ac4649315880387300158d7d756
Luciano Lopez <hey@lucianolopez.me>
2021-02-24 01:22:32 -0500

    Add style to PlantInfo

--- src/plants/PlantInfo.jsx
+++ src/plants/PlantInfo.jsx
@@ -1,11 +1,13 @@
+import styles from "./PlantInfo.module.css";
+
 /**
  * A component that displays a fun fact about a plant.
  */
 export const PlantInfo = (props) => {
-    return (
-      <div>
-        <h2>{props.name}</h2>
-        <p>{props.fact}</p>
-      </div>
-    );
-  };
+  return (
+    <div className={styles.plantinfo}>
+      <h2>{props.name}</h2>
+      <p className={styles.fact}>{props.fact}</p>
+    </div>
+  );
+};
