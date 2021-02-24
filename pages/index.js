20cfff254177aa256181f15c6f555c577945711d
Luciano Lopez <hey@lucianolopez.me>
2021-02-24 00:40:52 -0500

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+/**
+ * A component that displays a fun fact about a plant.
+ */
+const PlantInfo = (props) => {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <p>{props.fact}</p>
+    </div>
+  );
+};
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <PlantInfo name="Aloe" fact="Aloe has healing properties." />
+      <PlantInfo name="Lithops" fact="Lithops are known as mimicry plants." />
+    </div>
+  );
+}
