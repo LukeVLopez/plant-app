0f2646da52c671f9862900a9b02cb59f2fc9b7ce
Luciano Lopez <hey@lucianolopez.me>
2021-02-24 00:49:29 -0500

    Add daisies

--- pages/index.js
+++ pages/index.js
@@ -16,8 +16,12 @@ const PlantInfo = (props) => {
 export default function Home() {
   return (
     <div className={styles.container}>
-      <PlantInfo name="Aloe" fact="Aloe has healing properties." />
+      <PlantInfo name="Aloe" fact="Aloe has healing properties!" />
       <PlantInfo name="Lithops" fact="Lithops are known as mimicry plants." />
+      <PlantInfo
+        name="Daisies"
+        fact="If you don't compliment this site, you're gonna be pushin' them up."
+      />
     </div>
   );
 }
